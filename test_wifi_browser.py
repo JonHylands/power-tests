568ea45d1d2d815616f63918e857d2b2ce5fbac2
Jon Hylands <jhylands@mozilla.com>
2015-09-08 10:02:28 -0400

    minor changes, still not working

--- test_wifi_browser.py
+++ test_wifi_browser.py
@@ -13,9 +13,10 @@ class TestWifiBrowserPower(TestPower):
     name = 'Browser'
     manifest_url = "app://search.gaiamobile.org/manifest.webapp"
 
-    _url_bar_locator = (By.CSS_SELECTOR, 'div.search-app .urlbar .title')
-    _history_item_locator = (By.CSS_SELECTOR, '#history .result')
-    _private_window_locator = (By.ID, 'private-window')
+    #_url_bar_locator = (By.CSS_SELECTOR, 'div.search-app .urlbar .title')
+    _url_bar_locator = (By.ID, 'search-input')
+    #_history_item_locator = (By.CSS_SELECTOR, '#history .result')
+    #_private_window_locator = (By.ID, 'private-window')
 
 
     def setUp(self):
@@ -23,15 +24,26 @@ class TestWifiBrowserPower(TestPower):
         self.data_layer.set_setting("airplaneMode.enabled", False)
 
 
-    def go_to_url(self, url):
+    def go_to_url(self, homescreen, url):
         # The URL bar shown is actually in the system app not in this Search app.
         # We switch back to the system app, then tap the panel, but this will only
         # work from Search app which embiggens the input bar
-        self.marionette.switch_to_frame()
+#        self.marionette.switch_to_frame()
         self.marionette.find_element(*self._url_bar_locator).tap()
+        
+        #per https://github.com/mozilla-b2g/gaia/blob/master/tests/python/gaia-ui-tests/gaiatest/apps/homescreen/app.py#L35
+        # workaround for bug 1020974 
+        #import time
+        #time.sleep(1)
+        #self.marionette.switch_to_frame()
+        #Wait(self.marionette).until(lambda m: not self.keyboard.is_keyboard_displayed)
+        #self.marionette.find_element('id', 'rocketbar-form').tap()
+        #Wait(self.marionette).until(lambda m: self.keyboard.is_keyboard_displayed)
 
-        from gaiatest.apps.homescreen.regions.search_panel import SearchPanel
-        search_panel = SearchPanel(self.marionette)
+        #from gaiatest.apps.homescreen.regions.search_panel import SearchPanel
+        #search_panel = SearchPanel(self.marionette)
+        
+        search_panel = homescreen.tap_search_bar()
         return search_panel.go_to_url(url)
 
 
@@ -40,12 +52,14 @@ class TestWifiBrowserPower(TestPower):
 
         lock_screen = LockScreen(self.marionette)
         homescreen = lock_screen.unlock()
+        self.apps.switch_to_displayed_app()
 
         self.data_layer.connect_to_wifi()
         print "Connected to wifi"
 
+        # homescreen.launch()
         self.wait_for_condition(lambda m: self.apps.displayed_app.name == homescreen.name)
-        self.go_to_url("http://www.yahoo.com")
+        self.go_to_url(homescreen, "http://www.yahoo.com")
         print ""
         print "Running Wifi Browser Test (Yahoo)"
         self.runPowerTest("wifi_browser_yahoo", "Browser", "browser")
