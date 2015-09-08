ad5ad01d46840c8c08e45500af620b17bdf5bf9a
Jon Hylands <jhylands@mozilla.com>
2015-09-08 10:14:51 -0400

    finally working

--- test_wifi_browser.py
+++ test_wifi_browser.py
@@ -25,24 +25,7 @@ class TestWifiBrowserPower(TestPower):
 
 
     def go_to_url(self, homescreen, url):
-        # The URL bar shown is actually in the system app not in this Search app.
-        # We switch back to the system app, then tap the panel, but this will only
-        # work from Search app which embiggens the input bar
-#        self.marionette.switch_to_frame()
         self.marionette.find_element(*self._url_bar_locator).tap()
-        
-        #per https://github.com/mozilla-b2g/gaia/blob/master/tests/python/gaia-ui-tests/gaiatest/apps/homescreen/app.py#L35
-        # workaround for bug 1020974 
-        #import time
-        #time.sleep(1)
-        #self.marionette.switch_to_frame()
-        #Wait(self.marionette).until(lambda m: not self.keyboard.is_keyboard_displayed)
-        #self.marionette.find_element('id', 'rocketbar-form').tap()
-        #Wait(self.marionette).until(lambda m: self.keyboard.is_keyboard_displayed)
-
-        #from gaiatest.apps.homescreen.regions.search_panel import SearchPanel
-        #search_panel = SearchPanel(self.marionette)
-        
         search_panel = homescreen.tap_search_bar()
         return search_panel.go_to_url(url)
 
@@ -52,13 +35,11 @@ class TestWifiBrowserPower(TestPower):
 
         lock_screen = LockScreen(self.marionette)
         homescreen = lock_screen.unlock()
-        self.apps.switch_to_displayed_app()
 
         self.data_layer.connect_to_wifi()
         print "Connected to wifi"
 
-        # homescreen.launch()
-        self.wait_for_condition(lambda m: self.apps.displayed_app.name == homescreen.name)
+        self.apps.switch_to_displayed_app()
         self.go_to_url(homescreen, "http://www.yahoo.com")
         print ""
         print "Running Wifi Browser Test (Yahoo)"
